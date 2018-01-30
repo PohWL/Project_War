65351e75296f8dc105c7003d29dd5a4ddbfc210a
PohWL <PohWL@DESKTOP-552GTLP>
2018-01-31 03:44:51 +0800

    Revert "Added ECommerce_AddFurnitureToListServlet.java"
    This reverts commit 37051ae4c99f66407473462203d1f80488943bf5.

--- IS3102_Project-war/src/java/HelperClasses/ShoppingCartLineItem.java
+++ IS3102_Project-war/src/java/HelperClasses/ShoppingCartLineItem.java
@@ -1,9 +1,5 @@
 package HelperClasses;
 //###
-
-import java.sql.Time;
-import java.util.Date;
-
 public class ShoppingCartLineItem{
     private String id;
     private String SKU;
@@ -12,11 +8,6 @@ public class ShoppingCartLineItem{
     private double price;
     private int quantity;
     private long countryID;
-    private Date datePurchased;
-    private Time timePurchased;
-    private String storeName;
-    private String storeAddress;
-    private int orderId;
 
     public ShoppingCartLineItem() {
         this.id="";
@@ -26,11 +17,6 @@ public class ShoppingCartLineItem{
         this.price = 0.0;
         this.quantity = 0;
         this.countryID = 0;
-        this.datePurchased = null;
-        this.timePurchased = null;
-        this.storeName = "";
-        this.storeAddress = "";
-        this.orderId = 0;
     }
 
     public String getId() {
@@ -89,46 +75,6 @@ public class ShoppingCartLineItem{
         this.countryID = countryID;
     }
 
-    public Date getDatePurchased() {
-        return datePurchased;
-    }
-
-    public void setDatePurchased(Date datePurchased) {
-        this.datePurchased = datePurchased;
-    }
-    
-    public Time getTimePurchased() {
-        return timePurchased;
-    }
-
-    public void setTimePurchased(Time timePurchased) {
-        this.timePurchased = timePurchased;
-    }
-
-    public String getStoreName() {
-        return storeName;
-    }
-
-    public void setStoreName(String storeName) {
-        this.storeName = storeName;
-    }
-
-    public String getStoreAddress() {
-        return storeAddress;
-    }
-
-    public void setStoreAddress(String storeAddress) {
-        this.storeAddress = storeAddress;
-    }
-
-    public int getOrderId() {
-        return orderId;
-    }
-
-    public void setOrderId(int orderId) {
-        this.orderId = orderId;
-    }
-    
     @Override
     public boolean equals(Object obj) {
         boolean result = false;
